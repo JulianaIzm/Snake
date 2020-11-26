8bb4d633f6e69e29edc0568ab4c0f3b7a28d53fc
Juliana <50960477+JulianaIzm@users.noreply.github.com>
2020-11-26 13:36:38 +0300

    beginning

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ let canvas = document.querySelector('#canvas');
 let ctx = canvas.getContext('2d');
 let width = canvas.width;
 let height = canvas.height;
+class Snake = {
+    name: Snake
+    
+}
 
